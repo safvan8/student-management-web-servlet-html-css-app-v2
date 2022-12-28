090b59cf28df8d607f5886ffd32ac13680ecdf55
Safvan <safvanpmcz@gmail.com>
2022-12-28 16:50:53 +0530

    4th commit - all codes added

--- src/main/java/in/ineuron/controller/MainServlet.java
+++ src/main/java/in/ineuron/controller/MainServlet.java
@@ -1,4 +1,14 @@
 package in.ineuron.controller;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import in.ineuron.model.AllQueryGenerator;
+import in.ineuron.model.MySqlJdbcUtil;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -11,21 +21,57 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class MainServlet
  */
-@WebServlet(
-		urlPatterns = { "/mainServlet" }, 
-		initParams = { 
-				@WebInitParam(name = "jdbcURL", value = "jdbc:mysql://localhost:3306/schooldbo"), 
-				@WebInitParam(name = "username", value = "root"), 
-				@WebInitParam(name = "passwd", value = "Safvan@123")
-		})
-public class MainServlet extends HttpServlet {
+@WebServlet(urlPatterns = { "/mainServlet" }, initParams = {
+		@WebInitParam(name = "jdbcURL", value = "jdbc:mysql://localhost:3306/schooldbo"),
+		@WebInitParam(name = "username", value = "root"), @WebInitParam(name = "passwd", value = "Safvan@123") })
+public class MainServlet extends HttpServlet
+{
 	private static final long serialVersionUID = 1L;
+	Connection connection;
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	private AllQueryGenerator allQueryGenerator = AllQueryGenerator.getAllQueryGenerator();
+
+	PreparedStatement preparedStatementForInsert;
+
+	static
+	{
+		System.out.println("servlet class loading success");
+	}
+
+	public MainServlet()
+	{
+		System.out.println("servlet  instantiation success");
+	}
+
+	@Override
+	public void init()
+	{
+		System.out.println("Servlet initilization succes");
+	}
+
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+	{
+		// finding which operation is requested by user from html file
+		String dbOperation = request.getParameter("operation");
+
+		connection = MySqlJdbcUtil.getmySqlJdbcConnection();
+
+		if (dbOperation.equals("insert"))
+		{
+			// getting sql query based on the specified dbOperation
+			String sqlQuery = allQueryGenerator.generateSqlQuery(dbOperation);
+
+			if (connection != null)
+			{
+				// getting preparedStatement for insert operation from Util class
+				preparedStatementForInsert = MySqlJdbcUtil.getPreparedStatement(connection, sqlQuery);
+				
+				// setting user input values to the insert query
+				allQueryGenerator.setUserInputValuesToPreparedStatement(request, dbOperation, preparedStatementForInsert);
+				
+			}
+		}
 	}
 
 }
